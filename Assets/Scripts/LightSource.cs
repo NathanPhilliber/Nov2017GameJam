611a16d16154922b3a7390f3a1274869dc740e41
NathanPhilliber <nphilliber@gmail.com>
2017-11-10 20:15:00 -0800

    Light Source Object Tracking

--- Assets/Scripts/LightSource.cs
+++ Assets/Scripts/LightSource.cs
@@ -4,13 +4,63 @@ using UnityEngine;
 
 public class LightSource : MonoBehaviour {
 
-	// Use this for initialization
+	public float rayLength;
+
+	public LayerMask collisionMask;
+
+	[HideInInspector]
+	public float rangeMin;
+	[HideInInspector]
+	public float rangeMax;
+
+	public Light spotlight;
+
+	public List<GameObject> objectsInRange = new List<GameObject>();
+
 	void Start () {
-		
+		rangeMin = -(spotlight.spotAngle * .82f) / 100;
+		rangeMax = -rangeMin;
+			
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+
+		RayCheck ();
+		PrecisionCheckInRange ();
+			
+	}
+
+	void PrecisionCheckInRange(){
+		List<GameObject> removeAfter = new List<GameObject> ();
+		foreach (var obj in objectsInRange) {
+			Vector2 objVector = new Vector2 (obj.transform.position.x - transform.position.x, obj.transform.position.y - transform.position.y);
+			Debug.DrawRay (transform.position, objVector, Color.green);
+			float angle = Mathf.Deg2Rad*Vector2.Angle (objVector, Vector2.down);
+
+			if (angle < rangeMin || angle > rangeMax) {
+				removeAfter.Add (obj);
+			}
+		}
+		for (int i = removeAfter.Count - 1; i >= 0; i--) {
+			objectsInRange.Remove(removeAfter[i]);
+		}
+	}
+
+	void RayCheck(){
+		Vector2 minVector = new Vector2 (Mathf.Cos (rangeMin - Mathf.PI / 2), Mathf.Sin (rangeMin - Mathf.PI / 2));
+		Vector2 maxVector = new Vector2 (Mathf.Cos (rangeMax - Mathf.PI / 2), Mathf.Sin (rangeMax - Mathf.PI / 2));
+
+		Debug.DrawRay (transform.position, minVector*rayLength, Color.yellow);
+		Debug.DrawRay (transform.position, maxVector*rayLength, Color.yellow);
+
+		RaycastHit2D hitMin = Physics2D.Raycast (transform.position, minVector, rayLength, collisionMask);
+		RaycastHit2D hitMax = Physics2D.Raycast (transform.position, maxVector, rayLength, collisionMask);
+
+		if (hitMin || hitMax) {
+			GameObject obj = hitMin ? hitMin.transform.gameObject : hitMax.transform.gameObject;
+			if (objectsInRange.Contains (obj) == false) {
+				objectsInRange.Add (obj);
+			}
+		}
 	}
 }
